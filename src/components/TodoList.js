c52a4fd3154a6e1e6c55f8b8fe33a321f8078bb2
MaxAlieksieiev <corp.workMA@gmail.com>
2020-10-17 21:48:58 +0300

    separate TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import TodoListItem from './TodoListItem';
 
 const TodoList = () => {
 
-  const items = ['Build React', 'Build Awesome App'];
   return (
     <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
+      <li><TodoListItem/></li>
+      <li><TodoListItem/></li>
     </ul>
   )
 }
