391511062647a978184f3f3ab1d9b8bdf95a41f4
MaxAlieksieiev <corp.workMA@gmail.com>
2020-10-17 21:40:09 +0300

    create components & add editorconfig

---
+++ src/components/SearchPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchPanel = () => {
+
+  const searchText = 'Type to search';
+
+  return (
+    <input placeholder={searchText}/>
+  )
+}
+
+export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './components/AppHeader';
+import SearchPanel from './components/SearchPanel';
+import TodoList from './components/TodoList';
 
-const TodoList = () => {
-
-  const items = ['Build React', 'Build Awesome App'];
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-    </ul>
-  )
-}
-
-const AppHeader = () => {
-  return (
-    <h1>My Todo List</h1>
-  )
-}
-
-const SearchPanel = () => {
-
-  const searchText = 'Type to search';
-
-  return (
-    <input placeholder={searchText}/>
-  )
-}
 
 const App = () => {
   return (
